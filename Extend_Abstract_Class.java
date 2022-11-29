350e26bab51e3e5bffab4d3d9179748110b4287b
ram-wappnet <ram.wappnet@gmail.com>
2022-11-29 11:00:31 +0530

    added more details

--- Extend_Abstract_Class.java
+++ Extend_Abstract_Class.java
@@ -1,22 +1,17 @@
-abstract class MyClass {
-    public void disp() {
-        System.out.println("Concrete method of parent class");
-    }
-
-    abstract public void disp2();
-}
-
-class Demo extends MyClass {
-    /*
-     * Must Override this method while extending
-     * MyClas
-     */
-    public void disp2() {
-        System.out.println("overriding abstract method");
-    }
-
-    public static void main(String args[]) {
-        Demo obj = new Demo();
-        obj.disp2();
-    }
-}
+abstract class Shape{  
+abstract void draw();  
+}  
+//In real scenario, implementation is provided by others i.e. unknown by end user  
+class Rectangle extends Shape{  
+void draw(){System.out.println("drawing rectangle");}  
+}  
+class Circle1 extends Shape{  
+void draw(){System.out.println("drawing circle");}  
+}  
+//In real scenario, method is called by programmer or user  
+public class Extend_Abstract_Class{  
+public static void main(String args[]){  
+Shape s=new Circle1();//In a real scenario, object is provided through method, e.g., getShape() method  
+s.draw();  
+}  
+}  
