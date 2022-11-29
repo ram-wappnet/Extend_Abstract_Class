f5b63b8dfcad59b78fdf929a4ba5e0f64cf3c4cc
ram-wappnet <ram.wappnet@gmail.com>
2022-11-29 16:04:26 +0530

    Abstract demo done

--- Extend_Abstract_Class.java
+++ Extend_Abstract_Class.java
@@ -1,17 +1,9 @@
-abstract class Shape{  
-abstract void draw();  
-}  
-//In real scenario, implementation is provided by others i.e. unknown by end user  
-class Rectangle extends Shape{  
-void draw(){System.out.println("drawing rectangle");}  
-}  
-class Circle1 extends Shape{  
-void draw(){System.out.println("drawing circle");}  
-}  
-//In real scenario, method is called by programmer or user  
-public class Extend_Abstract_Class{  
-public static void main(String args[]){  
-Shape s=new Circle1();//In a real scenario, object is provided through method, e.g., getShape() method  
-s.draw();  
-}  
-}  
+
+public class Extend_Abstract_Class {
+    public static void main(String args[]) {
+
+        Pig myPig = new Pig(); // Create a Pig object
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Extend_Abstract_Class
+
+# brief details of the project
+
+In these project I have made a example of Abstract class where you can use abstract method.
+
+here i have created animal class it's paren class in this class we have Abstract method called animalSound().in Pig class we have extend Animal class and using abstract method
+
+# output of the project
+
+The pig says: wee wee
+Zzz
+
+# all steps of running the project
+
+**(1) git clone <ssh_url>** //to clone repository
+
+**(2) open repository in vs code editor**
+
+**(3)go to extension tab and download Extension pack for java extension**
+
+**(4)after adding extension you can see run button on right hand side press it and run**
